a544640ae17acc5f873bdda2464e68c969fc2216
= <=>
2025-07-04 10:12:42 +0530

    Added test files and removed DemoApplicationTests

---
+++ demo/src/test/java/com/example/demo/UserControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import com.example.userManagementAPI.Model.User;
+import com.example.userManagementAPI.Repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private UserRepository userRepository;
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    void setUp(){
+        userRepository.deleteAll();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        User initialUser1 = new User("John Doe", "john.doe@example.com");
+        User initialUser2 = new User("Jane Smith", "jane.smith@example.com");
+
+        List<User> savedInitialUsers = userRepository.saveAll(Arrays.asList(initialUser1, initialUser2));
+        this.user1 = savedInitialUsers;
+        this.user2
+    }
+
+}
